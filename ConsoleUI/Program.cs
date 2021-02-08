bf076d77d08b71b479ceb619b5c7efa589bbc6e8
gunnurraksoy@gmail.com <gunnurraksoy@gmail.com>
2021-02-08 19:03:52 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("CarId: " + car.CarId + " BrandId: " + car.BrandId + " ColorId: " + car.ColorId + " Model Year: " + car.ModelYear + " Daily Price: " + car.DailyPrice + " Description: " + car.Description);
+            }
+
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+
+
+            // Veritabanına yeni bir araba ekleyelim ve ekledikten sonra veritabanaındaki tüm arabaları GetAll ile çekelim.
+            InMemoryCarDal ınMemoryCarDal = new InMemoryCarDal();
+            ınMemoryCarDal.Add(new Entities.Concrete.Car { CarId = 6,BrandId=3,ColorId=3,ModelYear=2011,DailyPrice=225,Description="benzin"});
+            foreach (var car in ınMemoryCarDal.GetAll())
+            {
+                Console.WriteLine("CarId: " + car.CarId + " BrandId: " + car.BrandId + " ColorId: " + car.ColorId + " Model Year: " + car.ModelYear + " Daily Price: " + car.DailyPrice + " Description: " + car.Description);
+
+            }
+
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+            
+
+            //GetById metodu ile veritabanındaki 1 numaralı Id ye sahip olan arabanın tüm özelliklerini getirdik.
+            foreach (var car in ınMemoryCarDal.GetById(1))
+            {
+                Console.WriteLine("CarId: " + car.CarId + " BrandId: " + car.BrandId + " ColorId: " + car.ColorId + " Model Year: " + car.ModelYear + " Daily Price: " + car.DailyPrice + " Description: " + car.Description);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear=2015,DailyPrice=200,Description="dizel"},
+                new Car{CarId=2,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=400,Description="dizel"},
+                new Car{CarId=3,BrandId=2,ColorId=2,ModelYear=2017,DailyPrice=250,Description="benzin"},
+                new Car{CarId=4,BrandId=2,ColorId=3,ModelYear=2014,DailyPrice=180,Description="benzin"},
+                new Car{CarId=5,BrandId=3,ColorId=4,ModelYear=2018,DailyPrice=300,Description="dizel"},
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate= _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+
+        }
+    }
+}
